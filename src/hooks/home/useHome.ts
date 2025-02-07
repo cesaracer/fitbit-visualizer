4bdab13bdba4c35d4850084eff6ea247abc42bd1
devramoscesar <devramoscesar@gmail.com>
2025-02-06 19:29:56 -0700

    feat: connecting to backend lambdas

--- src/hooks/home/useHome.ts
+++ src/hooks/home/useHome.ts
@@ -1,43 +1,74 @@
-import { useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 function useHome() {
   const [activity, setActivity] = useState(null);
   const [isModalActive, setIsModalActive] = useState<boolean>(false);
 
-  const activities = useMemo(() => {
-    //GET DATA FROM FITBIT
-    let dateMap = {};
-    // const res = await axios.get("");
-    // const resData = res.data?.activities;
-    const resData = test.activities;
+  const handleRetrieveData = useCallback(async () => {
+    const res = await axios.get(
+      `${import.meta.env.VITE_API_BASE_URI}/activity/`,
+      {
+        headers: {
+          "x-api-key": `${import.meta.env.VITE_API_KEY}`,
+        },
+      }
+    );
 
-    resData.forEach((dataItem) => {
-      const date = new Date(dataItem.originalStartTime);
-      const year = date.getFullYear();
-      const month = date.getMonth();
-      const day = date.getDate();
-
-      const key = `${year}-${month}-${day}`;
-
-      const activityDate = {
-        steps: dataItem.steps,
-        calories: dataItem.calories,
-        activityName: dataItem.activityName,
-        date: key,
-        source: dataItem.source,
-      };
-
-      dateMap[key] = activityDate;
-    });
-
-    return dateMap;
+    return res.data.body;
   }, []);
 
-  async function handleRetrieveData() {
-    const res = await axios.get("");
-    return res.data;
-  }
+  const handleRetrieveDataByDate = useCallback(async (date: string) => {
+    const res = await axios.get(
+      `${import.meta.env.VITE_API_BASE_URI}/activity/date&date=${date}`,
+      {
+        headers: {
+          "x-api-key": `${import.meta.env.VITE_API_KEY}`,
+        },
+      }
+    );
+
+    return res.data.body;
+  }, []);
+
+  const { isError, isPending, isFetched, data } = useQuery({
+    queryKey: ["fitbit-data"],
+    queryFn: async () => {
+      const res = await handleRetrieveData();
+
+      return res;
+    },
+  });
+
+  const activities = useMemo(() => {
+    if (isFetched) {
+      console.log(data);
+      const fitbitData = data;
+
+      const durationData = [];
+
+      fitbitData?.forEach((fitbitDataItem) => {
+        const activityName = fitbitDataItem.activityName;
+        const duration = fitbitDataItem.duration / 1000 / 60;
+        const id = durationData.length;
+
+        const activityIndex = durationData.findIndex(
+          (x) => x.label === activityName
+        );
+        if (activityIndex !== -1) {
+          const activityItem = durationData[activityIndex];
+          activityItem.value = activityItem.value + duration;
+          durationData[activityIndex] = activityItem;
+        } else {
+          const activityItem = { id, value: duration, label: activityName };
+          durationData.push(activityItem);
+        }
+      });
+
+      return durationData;
+    }
+  }, [data, isFetched]);
 
   function handleRefreshToken() {
     //
@@ -65,136 +96,3 @@ function useHome() {
 }
 
 export { useHome };
-
-const test = {
-  pagination: {
-    afterDate: "2024-12-31",
-    limit: 1,
-    next: "https://api.fitbit.com/1/user/-/activities/list.json?offset=1&limit=1&sort=desc&afterDate=2024-12-31",
-    offset: 0,
-    previous: "",
-    sort: "desc",
-  },
-  activities: [
-    {
-      logId: 2907044112471568224,
-      activityTypeId: 90013,
-      activityName: "Walk",
-      calories: 158,
-      distance: 1.51788,
-      steps: 2272,
-      speed: 3.024,
-      pace: 1190.4761904761906,
-      averageHeartRate: 79,
-      duration: 1810000,
-      activeDuration: 1807000,
-      activityLevel: [
-        { minutes: 0, name: "sedentary" },
-        { minutes: 16, name: "lightly" },
-        { minutes: 6, name: "fairly" },
-        { minutes: 8, name: "very" },
-      ],
-      distanceUnit: "Kilometer",
-      source: {
-        type: "tracker",
-        name: "Google Pixel Watch 2",
-        id: "F59FD4504541",
-        url: "https://www.fitbit.com/",
-        trackerFeatures: [
-          "DISTANCE",
-          "STEPS",
-          "GPS",
-          "PACE",
-          "ELEVATION",
-          "CALORIES",
-          "HEARTRATE",
-          "VO2_MAX",
-        ],
-      },
-      logType: "tracker",
-      manualValuesSpecified: {
-        calories: false,
-        distance: false,
-        steps: false,
-      },
-      intervalWorkoutData: { intervalSummaries: [], numRepeats: 0 },
-      heartRateZones: [
-        {
-          minutes: 30,
-          caloriesOut: 158.359502,
-          name: "Out of Range",
-          min: 30,
-          max: 220,
-        },
-        {
-          minutes: 0,
-          caloriesOut: 0.0,
-          name: "Fat Burn",
-          min: 30,
-          max: 220,
-        },
-        {
-          minutes: 0,
-          caloriesOut: 0.0,
-          name: "Cardio",
-          min: 30,
-          max: 220,
-        },
-        {
-          minutes: 0,
-          caloriesOut: 0.0,
-          name: "Peak",
-          min: 30,
-          max: 220,
-        },
-      ],
-      activeZoneMinutes: {
-        totalMinutes: 0,
-        minutesInHeartRateZones: [
-          {
-            minutes: 0,
-            zoneName: "Out of Range",
-            order: 0,
-            type: "OUT_OF_ZONE",
-            minuteMultiplier: 0,
-          },
-          {
-            minutes: 0,
-            zoneName: "Fat Burn",
-            order: 1,
-            type: "FAT_BURN",
-            minuteMultiplier: 1,
-          },
-          {
-            minutes: 0,
-            zoneName: "Cardio",
-            order: 2,
-            type: "CARDIO",
-            minuteMultiplier: 2,
-          },
-          {
-            minutes: 0,
-            zoneName: "Peak",
-            order: 3,
-            type: "PEAK",
-            minuteMultiplier: 2,
-          },
-        ],
-      },
-      inProgress: false,
-      caloriesLink:
-        "https://api.fitbit.com/1/user/-/activities/calories/date/2025-02-03/2025-02-03/1min/time/11:57/12:28.json",
-      heartRateLink:
-        "https://api.fitbit.com/1/user/-/activities/heart/date/2025-02-03/2025-02-03/1sec/time/11:57:55/12:28:05.json",
-      tcxLink:
-        "https://api.fitbit.com/1/user/-/activities/2907044112471568224.tcx",
-      lastModified: "2025-02-03T19:28:27.656Z",
-      startTime: "2025-02-03T11:57:55.000-07:00",
-      originalStartTime: "2025-02-03T11:57:55.000-07:00",
-      originalDuration: 1810000,
-      elevationGain: 7.01,
-      hasActiveZoneMinutes: true,
-      hasGps: true,
-    },
-  ],
-};
