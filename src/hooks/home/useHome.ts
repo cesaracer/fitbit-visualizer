ed07c194d9f6dae8ab041b1530d489c18fce1032
devramoscesar <devramoscesar@gmail.com>
2025-02-07 18:27:30 -0700

    feat: connecting to new api route and rewriting logic

--- src/hooks/home/useHome.ts
+++ src/hooks/home/useHome.ts
@@ -1,13 +1,41 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 function useHome() {
-  const [activity, setActivity] = useState(null);
+  const [activityData, setActivityData] = useState(null);
   const [isModalActive, setIsModalActive] = useState<boolean>(false);
 
+  function handleDataParse(data) {
+    console.log(data);
+    const fitbitData = data;
+
+    const durationData = [];
+
+    fitbitData?.forEach((fitbitDataItem) => {
+      const activityName = fitbitDataItem.activityName;
+      const duration = fitbitDataItem.duration / 1000 / 60;
+      const id = durationData.length;
+
+      const activityIndex = durationData.findIndex(
+        (x) => x.label === activityName
+      );
+      if (activityIndex !== -1) {
+        const activityItem = durationData[activityIndex];
+        activityItem.value = activityItem.value + duration;
+        durationData[activityIndex] = activityItem;
+      } else {
+        const activityItem = { id, value: duration, label: activityName };
+        durationData.push(activityItem);
+      }
+    });
+
+    setActivityData(durationData);
+  }
+
   const handleRetrieveData = useCallback(async () => {
-    const res = await axios.get(
+    let data = [];
+    let res = await axios.get(
       `${import.meta.env.VITE_API_BASE_URI}/activity/`,
       {
         headers: {
@@ -16,62 +44,65 @@ function useHome() {
       }
     );
 
-    return res.data.body;
+    if (res.data.statusCode === 500) {
+      handleRefreshToken();
+      let retry = 0;
+      while (retry < 3) {
+        res = await axios.get(
+          `${import.meta.env.VITE_API_BASE_URI}/activity/`,
+          {
+            headers: {
+              "x-api-key": `${import.meta.env.VITE_API_KEY}`,
+            },
+          }
+        );
+
+        if (res.data.statusCode !== 500) {
+          data = res.data.body.activities;
+          break;
+        }
+
+        retry++;
+      }
+    } else {
+      data = res.data.body.activities;
+    }
+
+    handleDataParse(data);
   }, []);
 
-  const handleRetrieveDataByDate = useCallback(async (date: string) => {
-    const res = await axios.get(
-      `${import.meta.env.VITE_API_BASE_URI}/activity/date&date=${date}`,
+  const dateMutation = useMutation({
+    mutationKey: ["fitbit-date-data"],
+    mutationFn: async (date) => {
+      const month = date.$M + 1 < 10 ? `0${date.$M + 1}` : `${date.$M + 1}`;
+      const day = date.$D < 10 ? `0${date.$D}` : `${date.$D}`;
+      const dateStr = `${date.$y}-${month}-${day}`;
+
+      console.log(dateStr);
+
+      const res = await axios.get(
+        `${import.meta.env.VITE_API_BASE_URI}/date?date=${dateStr}`,
+        {
+          headers: {
+            "x-api-key": `${import.meta.env.VITE_API_KEY}`,
+          },
+        }
+      );
+
+      return res.data.activities;
+    },
+  });
+
+  async function handleRefreshToken() {
+    await axios.post(
+      `${import.meta.env.VITE_API_BASE_URI}/credentials`,
+      {},
       {
         headers: {
           "x-api-key": `${import.meta.env.VITE_API_KEY}`,
         },
       }
     );
-
-    return res.data.body;
-  }, []);
-
-  const { isError, isPending, isFetched, data } = useQuery({
-    queryKey: ["fitbit-data"],
-    queryFn: async () => {
-      const res = await handleRetrieveData();
-
-      return res;
-    },
-  });
-
-  const activities = useMemo(() => {
-    if (isFetched) {
-      console.log(data);
-      const fitbitData = data;
-
-      const durationData = [];
-
-      fitbitData?.forEach((fitbitDataItem) => {
-        const activityName = fitbitDataItem.activityName;
-        const duration = fitbitDataItem.duration / 1000 / 60;
-        const id = durationData.length;
-
-        const activityIndex = durationData.findIndex(
-          (x) => x.label === activityName
-        );
-        if (activityIndex !== -1) {
-          const activityItem = durationData[activityIndex];
-          activityItem.value = activityItem.value + duration;
-          durationData[activityIndex] = activityItem;
-        } else {
-          const activityItem = { id, value: duration, label: activityName };
-          durationData.push(activityItem);
-        }
-      });
-
-      return durationData;
-    }
-  }, [data, isFetched]);
-
-  function handleRefreshToken() {
-    //
   }
 
   function viewDetails() {
@@ -82,14 +113,19 @@ function useHome() {
     //
   }
 
+  useEffect(() => {
+    handleRetrieveData();
+  }, []);
+
   return {
     actions: {
       toggleModal,
       viewDetails,
+      searchByDate: dateMutation.mutate,
+      isSearchPending: dateMutation.isPending,
     },
     state: {
-      activities,
-      activity,
+      activities: activityData,
       isModalActive,
     },
   };
