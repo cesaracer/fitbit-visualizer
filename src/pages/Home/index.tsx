47d0e9a0a0581e3dd40fd3ce2afaebfbb14594db
devramoscesar <devramoscesar@gmail.com>
2025-02-08 09:36:24 -0700

    feat: implemented new font and completed base styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,6 +9,9 @@ import {
   Avatar,
   Card,
   Container,
+  Divider,
+  LinearProgress,
+  Link,
   List,
   ListItem,
   ListItemAvatar,
@@ -19,6 +22,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 
 function Home() {
@@ -28,138 +32,172 @@ function Home() {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-        <Container>
-          <Card
-            style={{ minHeight: 400, display: "flex", alignItems: "center" }}
+      <Container>
+        <Typography variant="h1">My Fitbit Activity</Typography>
+        <Divider />
+        <br />
+        <br />
+        <Card style={{ minHeight: 400, display: "flex", alignItems: "center" }}>
+          <div
+            style={{
+              flex: 1,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              height: "100%",
+            }}
           >
-            <div
-              style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-around",
-                height: "100%",
-              }}
-            >
-              <h3 style={{ textAlign: "center", height: 40 }}>
-                {} Workout Summary
-              </h3>
-              <List style={{ minHeight: 250 }}>
-                {dateData.isSuccess &&
-                  dateData.data.map((dateItem) => {
-                    const imgSrc = image_map[dateItem.activityParentName]
-                      ? image_map[dateItem.activityParentName]
-                      : image_map.other;
-
-                    return (
-                      <ListItem>
-                        <ListItemAvatar>
-                          <Avatar src={imgSrc} />
-                        </ListItemAvatar>
-                        <ListItemText>
-                          {dateItem.activityParentName} for{" "}
-                          {Math.trunc(dateItem.duration / 1000 / 60)} minutes
-                        </ListItemText>
-                      </ListItem>
-                    );
-                  })}
-              </List>
-            </div>
-            <div style={{ flex: 1 }}>
-              <DateCalendar onChange={(val) => searchByDate(val)} />
-            </div>
-          </Card>
-          <br />
-          <Card style={{ minHeight: 400, padding: 10 }}>
-            <h2>Recent Workout Trends</h2>
-            {activities && (
-              <PieChart
-                height={400}
-                series={[
-                  {
-                    data: activities,
-                    innerRadius: 30,
-                    outerRadius: 150,
-                    paddingAngle: 2,
-                    cornerRadius: 4,
-                    highlightScope: { fade: "global", highlight: "item" },
-                    faded: {
-                      innerRadius: 30,
-                      additionalRadius: -30,
-                      color: "gray",
-                    },
-                  },
-                ]}
-              />
+            {!dateData.isSuccess && !dateData.isPending && (
+              <Typography style={{ textAlign: "center" }}>
+                Use the calendar to view the workout summary for that date
+              </Typography>
             )}
-            <TableContainer>
-              <Table>
-                <TableHead>
-                  <TableRow style={{ backgroundColor: "black" }}>
-                    <TableCell style={{ color: "white", textAlign: "center" }}>
-                      Rank
-                    </TableCell>
-                    <TableCell style={{ color: "white", textAlign: "center" }}>
-                      Exercise
-                    </TableCell>
-                    <TableCell style={{ color: "white", textAlign: "center" }}>
-                      Total Minutes
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {activities &&
-                    activities.map((activity, index) => (
-                      <TableRow key={activity.id}>
-                        <TableCell style={{ textAlign: "center" }}>
-                          {index + 1}
-                        </TableCell>
-                        <TableCell style={{ textAlign: "center" }}>
-                          {activity.label}
-                        </TableCell>
-                        <TableCell style={{ textAlign: "center" }}>
-                          {activity.value}
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Card>
-          <br />
-          <br />
 
-          <Card style={{ padding: 10 }}>
-            <>
-              <a
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "flex-end",
-                  alignSelf: "flex-end",
-                }}
-                href=""
-                target="__blank"
-              >
-                <img
-                  src={logo}
-                  style={{ height: 30, width: 30, marginRight: 10 }}
-                />
-                Source Code
-              </a>
-            </>
-          </Card>
-          <br />
-        </Container>
-      </div>
+            {dateData.isPending && (
+              <LinearProgress style={{ marginLeft: 10 }} />
+            )}
+
+            {dateData.isSuccess && (
+              <>
+                <Typography
+                  style={{ textAlign: "center", justifySelf: "flex-start" }}
+                  variant="h4"
+                >
+                  Workout Summary
+                </Typography>
+                <List>
+                  {dateData.isSuccess &&
+                    dateData.data.map((dateItem) => {
+                      const imgSrc = image_map[dateItem.activityParentName]
+                        ? image_map[dateItem.activityParentName]
+                        : image_map.other;
+
+                      return (
+                        <ListItem>
+                          <div style={{ flex: 2, display: "flex" }}>
+                            <ListItemAvatar>
+                              <Avatar src={imgSrc} />
+                            </ListItemAvatar>
+                            <ListItemText>
+                              <Typography variant="h6">
+                                {dateItem.activityParentName}
+                              </Typography>
+                              {Math.trunc(dateItem.duration / 1000 / 60)}{" "}
+                              minutes
+                            </ListItemText>
+                          </div>
+
+                          <div style={{ flex: 1, display: "flex" }}>
+                            <ListItemText style={{}}>
+                              <Typography variant="subtitle1">
+                                Calories
+                              </Typography>
+                              <Typography variant="body1">
+                                {dateItem.calories}
+                              </Typography>
+                            </ListItemText>
+                          </div>
+                        </ListItem>
+                      );
+                    })}
+                </List>
+              </>
+            )}
+          </div>
+          <div style={{ flex: 1 }}>
+            <DateCalendar
+              disabled={dateData.isPending}
+              onChange={(val) => searchByDate(val)}
+            />
+          </div>
+        </Card>
+        <br />
+        <Card style={{ minHeight: 400 }}>
+          <Typography style={{ padding: 15 }} variant="h4">
+            Recent Workout Trends
+          </Typography>
+          {activities && (
+            <PieChart
+              height={400}
+              series={[
+                {
+                  data: activities,
+                  innerRadius: 30,
+                  outerRadius: 150,
+                  paddingAngle: 2,
+                  cornerRadius: 4,
+                  highlightScope: { fade: "global", highlight: "item" },
+                  faded: {
+                    innerRadius: 30,
+                    additionalRadius: -30,
+                    color: "gray",
+                  },
+                },
+              ]}
+            />
+          )}
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow style={{ backgroundColor: "black" }}>
+                  <TableCell style={{ color: "white", textAlign: "center" }}>
+                    Rank
+                  </TableCell>
+                  <TableCell style={{ color: "white", textAlign: "center" }}>
+                    Exercise
+                  </TableCell>
+                  <TableCell style={{ color: "white", textAlign: "center" }}>
+                    Total Minutes
+                  </TableCell>
+                  <TableCell style={{ color: "white", textAlign: "center" }}>
+                    Frequency
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {activities &&
+                  activities.map((activity, index) => (
+                    <TableRow key={activity.id}>
+                      <TableCell style={{ textAlign: "center" }}>
+                        {index + 1}
+                      </TableCell>
+                      <TableCell style={{ textAlign: "center" }}>
+                        {activity.label}
+                      </TableCell>
+                      <TableCell style={{ textAlign: "center" }}>
+                        {activity.value}
+                      </TableCell>
+                      <TableCell style={{ textAlign: "center" }}>
+                        {activity.frequency}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Card>
+        <br />
+        <br />
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-end",
+            position: "fixed",
+            bottom: 10,
+            right: 10,
+          }}
+        >
+          <img src={logo} style={{ height: 30, width: 30, marginRight: 10 }} />
+          <Link
+            href="https://github.com/cesaracer/fitbit-visualizer"
+            target="_blank"
+          >
+            <Typography>Source Code</Typography>
+          </Link>
+        </div>
+        <br />
+      </Container>
     </LocalizationProvider>
   );
 }
