7b26567be985ebbb4efe77e55beff8fed9f95ed7
devramoscesar <devramoscesar@gmail.com>
2025-02-07 22:06:36 -0700

    assets: added images and image map

---
+++ src/constants/image-map.ts
@@ -0,0 +1,13 @@
+export const image_map = {
+  Treadmill: "src/assets/treadmill.gif",
+  Walk: "src/assets/walk.gif",
+  "Strength training": "src/assets/muscle.gif",
+  Stairclimber: "src/assets/stairs.gif",
+  Weights: "src/assets/weight.gif",
+  Weightlifting: "src/assets/weightlifting.gif",
+  "Core training": "src/assets/abs,gif",
+  "Rowing machine": "src/assets/row.png",
+  Spinning: "src/assets/bike.gif",
+  Elliptical: "src/assets/eliptical.png",
+  other: "src/assets/weight.gif",
+};
